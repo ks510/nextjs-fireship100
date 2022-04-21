7a2d3413cefe94bbf529ef8c198950f892c7a64a
Karen <ksuen.dev@gmail.com>
2022-04-21 23:07:32 +0100

    feat(cars): add list placeholder and dynamic route component

--- pages/cars/[id].js
+++ pages/cars/[id].js
@@ -1,3 +1,8 @@
-export default function Car() {
+import { useRouter } from 'next/router';
 
+export default function Car() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  return <h1>Hello {id}</h1>;
 }
