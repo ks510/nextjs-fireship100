15ca7855d6948d082651c81028a6186e20cb34a3
Karen <ksuen.dev@gmail.com>
2022-04-22 00:12:59 +0100

    feat(cars): add SSG and SSR for next

--- pages/cars/[id].js
+++ pages/cars/[id].js
@@ -1,8 +1,47 @@
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 
-export default function Car() {
+export default function Car({ car }) {
   const router = useRouter();
   const { id } = router.query;
 
-  return <h1>Hello {id}</h1>;
+  return (<>
+    <Head>
+      <title>{car.color}</title>
+    </Head>
+    <h1>Hello {id}</h1>
+    <img src={car.image} />
+  </>);
+}
+
+/*export async function getServerSideProps({ params }) {
+  const req = await fetch(`http://localhost:3000/${params.id}.json`);
+  const data = await req.json();
+
+  return {
+    props: { car: data }
+  };
+}*/
+
+export async function getStaticProps(context) {
+  const req = await fetch(`http://localhost:3000/${context.params.id}.json`);
+  const data = await req.json();
+
+  return {
+    props: { car: data }
+  };
+}
+
+export async function getStaticPaths() {
+  const req = await fetch('http://localhost:3000/cars.json');
+  const data = await req.json();
+
+  const paths = data.map(car => {
+    return { params: { id: car } };
+  });
+
+  return {
+    paths,
+    fallback: false
+  };
 }
